6b759e4ce71ec5ff76153ba59dff000064729173
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-08 15:16:41 +0530

    feat(todos): implement GET/todos to get all todos

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 # ORM models -> database tables
 import models
 # DB connection object in database.py
-from database import engine
+from database import engine, SessionLocal
+from models import Todos
 
 app = FastAPI()
 
@@ -11,3 +15,24 @@ app = FastAPI()
 # creates table automatically if it doesn't exist
 # bind=engine -> specify which database to create tables
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+'''Dependency Injection (DI) is a design pattern where you define dependencies (like DB, Auth, Config) separately,
+and let the framework (FastAPI) inject them automatically when needed.'''
+# Dependency Injection
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/todos")
+# async def get_todos(db: Session = Depends(get_db)):
+async def get_todos(db: db_dependency):
+    todos = db.query(Todos).all()
+    return todos
