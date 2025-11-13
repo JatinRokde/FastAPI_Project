deca492decdbd2f349dfe51765d74b8f68f90766
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-13 23:48:14 +0530

    refactor(auth): modify get_current_user to return User object

--- routers/auth.py
+++ routers/auth.py
@@ -134,18 +134,27 @@ def create_access_token(data: dict, expires_delta: timedelta):
     return encoded_jwt
 
 
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: db_dependency):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or expired token.",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if not username or not user_id:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token.")
-
-        return {'username': username, 'id': user_id}
+            raise credentials_exception
 
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired token.")
+        raise credentials_exception
+
+    user = db.query(User).filter((User.id == user_id) and (User.username == username)).first()
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 @router.post("/token", response_model=TokenResponse)
