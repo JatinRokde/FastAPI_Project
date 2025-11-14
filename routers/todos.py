9f4e4f19a9cc22bd0d5a93fefeb6cadfae53048c
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-15 01:00:34 +0530

    feat(todos): add user dependency to PUT/todos/:todo_id

--- routers/todos.py
+++ routers/todos.py
@@ -81,8 +81,11 @@ async def add_todo(user: user_dependency, db: db_dependency, todo_req: TodoCreat
 
 
 @router.put("/todos/{todo_id}", status_code=status.HTTP_200_OK)
-async def update_todo(db: db_dependency, todo_req: TodoCreate, todo_id: int = Path(gt=0)):
-    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+async def update_todo(user: user_dependency, db: db_dependency, todo_req: TodoCreate, todo_id: int = Path(gt=0)):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Authentication Failed!")
+
+    todo = db.query(Todos).filter(Todos.id == todo_id, Todos.user_id == user.id).first()
     if todo is None:
         raise HTTPException(status_code=404, detail="Todo not found!")
 
