9285240e4b72b8d2032f70065c257c4284bb7033
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-15 22:15:59 +0530

    feat(admin): implement DELETE/todos/:todo_id to delete a todo by admin

--- routers/admin.py
+++ routers/admin.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, APIRouter
+from fastapi import Depends, HTTPException, APIRouter, Path
 from sqlalchemy.orm import Session
 from starlette import status
 
@@ -42,3 +42,14 @@ admin_dependency = Annotated[User, Depends(admin_required)]
 async def get_todos(db: db_dependency, admin: admin_dependency):
     todos = db.query(Todos).all()
     return todos
+
+
+@router.delete("/todos/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(admin: admin_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    todo = db.query(Todos).filter(Todos.id == todo_id).first()
+
+    if todo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Todo not found!')
+
+    db.delete(todo)
+    db.commit()
