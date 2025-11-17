a6b77c7fa9053a4fc4fe72ae94f7184442bc0b76
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-18 00:13:21 +0530

    chore(db): migrate database from SQLite to PostgreSQL

--- database.py
+++ database.py
@@ -1,6 +1,10 @@
 # create_engine: Creates connection to the DB
 from sqlalchemy import create_engine
 
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
 # declarative_base: ORM models (tables) in a class-based way
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,10 +12,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # DB URI (/// -> relative path)
-SQLALCHEMY_DATABASE_URI = 'sqlite:///todosapp.db'
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+if not SQLALCHEMY_DATABASE_URI:
+    raise ValueError("DATABASE_URL environment variable not set!")
 
 # engine: core interface to the DB, manages connection pool and interacts with directly to the DB (connects FastAPIs to the DB)
-engine = create_engine(SQLALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 # sessionmaker: factory that creates DB session objects i.e. provides session each time when wants to interact with DB
 # (bind=engine): links the session to the previously created engine, so all session use the same DB connection
