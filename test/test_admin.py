28b6ff7f19d618a1c7b0ae421284e2c5d60d67da
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-21 20:21:59 +0530

    test(pytest-admin): add test to verify admin deleting a todo

--- test/test_admin.py
+++ test/test_admin.py
@@ -13,3 +13,15 @@ def test_admin_get_all_todos(test_todo):
     todo = response.json()[0]
     assert todo["title"] == "Test Todo"
     assert todo["description"] == "Test Description"
+
+
+def test_admin_delete_todo(test_todo):
+    todo_id = test_todo.id
+
+    response = client.delete(f"/admin/todos/{todo_id}")
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+    # verify deletion
+    verify = client.get("/admin/todos")
+    todos = verify.json()
+    assert len(todos) == 0
