8d33c1a12c7245110ddedd9abee886d21f429def
Jatin Rokde <jar.jatin.rokde2000@gmail.com>
2025-11-27 00:16:32 +0530

    test(pytest-auth): add test to test the current user

--- test/test_auth.py
+++ test/test_auth.py
@@ -28,3 +28,20 @@ def test_login_success(test_user):
 
     assert decoded["sub"] == test_user.username
     assert decoded["id"] == test_user.id
+
+
+def test_get_current_user(test_user):
+    login_data = {
+        "username": test_user.username,
+        "password": "oldpassword"
+    }
+    login_res = client.post("/auth/token", data=login_data)
+    token = login_res.json()["access_token"]
+
+    response = client.get(
+        "/user/active_user",
+        headers={"Authorization": f"Bearer {token}"}
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()["username"] == test_user.username
